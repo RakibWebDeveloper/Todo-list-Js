12000c286890885ef3eac0e688ad23cd90ab73b7
RakibWebDeveloper <abdurrakib961@gmail.com>
2020-03-08 09:41:43 +0600

    Customized stylesheet

--- js/script.js
+++ js/script.js
@@ -84,7 +84,7 @@ document.addEventListener("keyup",function(even){
                 trash : false
             });
             
-            // add item to localstorage ( this code must be added where the LIST array is updated)
+            // add item to local storage ( this code must be added where the LIST array is updated)
             localStorage.setItem("TODO", JSON.stringify(LIST));
             
             id++;
